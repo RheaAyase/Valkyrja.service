551473a8a9a01c80e4c4af7774505f23551ef085
Radka Janek <radka.janek@redhat.com>
2018-08-06 21:19:55 +0200

    +raid status

--- Bot.cs
+++ Bot.cs
@@ -87,13 +87,17 @@ namespace Botwinder.Service
 						string[] cpuTemp = Bash.Run("sensors | grep Package | sed 's/Package id [01]:\\s*+//g' | sed 's/\\s*(high = +85.0°C, crit = +95.0°C)//g'").Split('\n');
 						string cpuLoad = Bash.Run("grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}'");
 						string memoryUsed = Bash.Run("free | grep Mem | awk '{print $3/$2 * 100.0}'");
+						string raidSync = Bash.Run("lvs raid5 -o 'lv_name,copy_percent,vg_missing_pv_count' | grep raid5 | awk '{print $2}'");
+						string raidFailedDrives = Bash.Run("lvs raid5 -o 'lv_name,copy_percent,vg_missing_pv_count' | grep raid5 | awk '{print $3}'");
 						string message = "Server Status: <http://status.botwinder.info>\n" +
-						                 $"```md\n[  Last update ][ {Utils.GetTimestamp(DateTime.UtcNow)} ]\n" +
-						                 $"[ Memory usage ][ {double.Parse(memoryUsed):#00.00} %                 ]\n" +
-						                 $"[     CPU Load ][ {double.Parse(cpuLoad):#00.00} %                 ]\n" +
-						                 $"[    CPU0 Temp ][ {cpuTemp[0]}                  ]\n" +
-						                 $"[    CPU1 Temp ][ {cpuTemp[1]}                  ]\n" +
-						                 $"[      Threads ][ {globalCount.ThreadsActive:#000}                     ]\n" +
+						                 $"```md\n[   Last update ][ {Utils.GetTimestamp(DateTime.UtcNow)} ]\n" +
+						                 $"[  Memory usage ][ {double.Parse(memoryUsed):#00.00} %                 ]\n" +
+						                 $"[      CPU Load ][ {double.Parse(cpuLoad):#00.00} %                 ]\n" +
+						                 $"[     CPU0 Temp ][ {cpuTemp[0]}                  ]\n" +
+						                 $"[     CPU1 Temp ][ {cpuTemp[1]}                  ]\n" +
+						                 $"[       Threads ][ {globalCount.ThreadsActive:#000}                     ]\n" +
+						                 $"[     Raid Sync ][ {double.Parse(raidSync):000.00} %                ]\n" +
+						                 $"[ Raid Failures ][ {int.Parse(raidFailedDrives):0}                       ]\n" +
 						                 $"```\n" +
 						                 $"**Shards: `{dbContext.Shards.Count()}`**\n\n" +
 						                 $"{shards.ToString()}";
