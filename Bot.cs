72c735ac7a145f73b1d703a19c431da4f84859a8
RheaAyase <radka.janek@redhat.com>
2020-02-20 11:35:30 +0100

    remove accidentally pushed animals

--- Bot.cs
+++ Bot.cs
@@ -209,12 +209,9 @@ namespace Valkyrja.service
 							}
 
 							string modifiedMessage = message;
-							Console.WriteLine("Animal does what?");
 							if( !this.ShuttingDown && this.Config.PrintShardsOnGuildId == server.GuildId )
 							{
-								Console.WriteLine("cow");
 								modifiedMessage += shards.ToString();
-								Console.WriteLine("moo");
 							}
 
 							try
