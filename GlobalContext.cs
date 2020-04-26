0720f226234e0e71b29f6d698356e61e1d820c57
RheaAyase <radka.janek@redhat.com>
2020-04-26 22:27:18 +0200

    updated to dotnet 3.1

--- GlobalContext.cs
+++ GlobalContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 
 using guid = System.UInt64;
 
@@ -7,13 +6,11 @@ namespace Valkyrja.entities
 {
 	public class GlobalContext: DbContext
 	{
-		public DbSet<Shard> Shards;
-		public DbSet<ServerConfig> ServerConfigurations;
+		public DbSet<Shard> Shards{ get; set; }
+		public DbSet<ServerConfig> ServerConfigurations{ get; set; }
 
-		public GlobalContext(DbContextOptions<GlobalContext> options) : base(options)
+		private GlobalContext(DbContextOptions<GlobalContext> options) : base(options)
 		{
-			this.Shards = new InternalDbSet<Shard>(this);
-			this.ServerConfigurations = new InternalDbSet<ServerConfig>(this);
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
